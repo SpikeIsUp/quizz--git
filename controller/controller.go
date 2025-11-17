1047ba7dbd31d4bb90e3c18d496e973ebe284d52
Quentin <quentin.guyenot@ynov.com>
2025-11-17 21:44:06 +0100

    cration du rooter

--- controller/controller.go
+++ controller/controller.go
@@ -13,3 +13,24 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) error {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	return (nil)
 }
+
+func Cyber(w http.ResponseWriter, r *http.Request) {
+	log.Println("➡️  GET /Cyber")
+	if err := RenderTemplate(w, "menu.html"); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func IAdata(w http.ResponseWriter, r *http.Request) {
+	log.Println("➡️  GET /IAdata")
+	if err := RenderTemplate(w, "menu.html"); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func Info(w http.ResponseWriter, r *http.Request) {
+	log.Println("➡️  GET /info")
+	if err := RenderTemplate(w, "menu.html"); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
--- main.go
+++ main.go
@@ -2,24 +2,51 @@ package main
 
 import (
 	"net/http"
-
+	"fmt"
+	"log"
 	"github.com/SpikelsUp/quizz--git/controller"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		if err := controller.RenderTemplate(w, "menu.html"); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+func cyberHandler(w http.ResponseWriter, r *http.Request) {
+	controller.Cyber(w, r)
+}
+
+func IAdataHandler(w http.ResponseWriter, r *http.Request) {
+	controller.IAdata(w, r)
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	controller.Info(w, r)
+}
+
 func main() {
-	ShowMenu()
-}
+	//cwd, _ := os.Getwd()
 
-func ShowMenu() {
-	panic("unimplemented")
-}
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-func menuHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/cyber", cyberHandler)
+	http.HandleFunc("/IAdata", IAdataHandler)
+	http.HandleFunc("/info", infoHandler)
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	addr := ":8080"
+	fmt.Printf("✅ Serveur lancé sur http://localhost%v\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
 	}
-	if err := controller.RenderTemplate(w, "menu.html"); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+
 }
